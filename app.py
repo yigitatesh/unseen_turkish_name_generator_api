9d041c25f2f306f17a706bf769502c76db3d62e8
yigitatesh <yigitatesh0312@gmail.com>
2021-06-22 01:04:16 +0300

    utf-8 encoding error solved 2

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 ### Load Names Data
 
-with open("data/names.txt", encoding="ISO-8859-1") as f:
+with open("data/names.txt", encoding=None) as f:
     names = f.readlines()
 
 ### Get info from Names Data
